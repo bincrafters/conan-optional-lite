706be0680570203c2251193cd029a54257719e6e
SSE4 <tomskside@gmail.com>
2019-06-21 11:15:29 +0200

    - apply bincrafters conventions
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,9 @@ class OptionalLiteConan(ConanFile):
     name = "optional-lite"
     version = "2.3.0"
     url = "https://github.com/bincrafters/conan-optional-lite"
+    homepage = "https://github.com/martinmoene/optional-lite"
     description = "A single-file header-only version of a C++17-like optional, a nullable object for C++98, C++11 and later"
+    author = "Bincrafters <bincrafters@gmail.com>"
 
     license = "MIT"
     exports = ["LICENSE.md"]
@@ -17,30 +19,31 @@ class OptionalLiteConan(ConanFile):
     generators = "cmake"
     settings = "os", "arch", "compiler", "build_type"
     options = {"build_tests": [True, False]}
-    default_options = "build_tests=True"
+    default_options = {'build_tests': 'True'}
+    topics = ("conan", "optional")
 
-    source_subfolder = "source_subfolder"
-    build_subfolder = "build_subfolder"
+    _source_subfolder = "source_subfolder"
+    _build_subfolder = "build_subfolder"
 
     def source(self):
-        source_url = "https://github.com/martinmoene/optional-lite"
-        tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version))
+        tools.get("{0}/archive/v{1}.tar.gz".format(self.homepage, self.version),
+                  sha256="8fe46216147234b172c6a5b182726834afc44dfdca1e976a264d6f96eb183916")
         extracted_dir = self.name + "-" + self.version
-        os.rename(extracted_dir, self.source_subfolder)
+        os.rename(extracted_dir, self._source_subfolder)
 
 
     def build(self):
         cmake = CMake(self)
         cmake.definitions["BUILD_TESTS"] = self.options.build_tests
-        cmake.configure(build_folder=self.build_subfolder)
+        cmake.configure(build_folder=self._build_subfolder)
         cmake.build()
         if self.options.build_tests:
-            path_to_test_exe = os.path.join(self.build_subfolder, 'bin', 'optional-lite.t')
+            path_to_test_exe = os.path.join(self._build_subfolder, 'bin', 'optional-lite.t')
             self.run(path_to_test_exe)
 
     def package(self):
-        include_folder = os.path.join(self.source_subfolder, "include")
-        self.copy(pattern="LICENSE", dst="license", src=self.source_subfolder)
+        include_folder = os.path.join(self._source_subfolder, "include")
+        self.copy(pattern="LICENSE", dst="license", src=self._source_subfolder)
         self.copy(pattern="*", dst="include", src=include_folder)
 
 
